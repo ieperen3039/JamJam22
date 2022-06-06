346e790d11adac853a54f9da1d9298e0fa9c3f17
Jos Demmers <josdemmers@gmail.com>
2022-06-06 17:31:18 +0200

    removed namespace

--- Assets/Scripts/MainUIController.cs
+++ Assets/Scripts/MainUIController.cs
@@ -2,28 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Platformer.UI
+/// <summary>
+/// A simple controller for switching between UI panels.
+/// </summary>
+public class MainUIController : MonoBehaviour
 {
-    /// <summary>
-    /// A simple controller for switching between UI panels.
-    /// </summary>
-    public class MainUIController : MonoBehaviour
+    public GameObject[] panels;
+
+    public void SetActivePanel(int index)
     {
-        public GameObject[] panels;
-
-        public void SetActivePanel(int index)
+        for (var i = 0; i < panels.Length; i++)
         {
-            for (var i = 0; i < panels.Length; i++)
-            {
-                var active = i == index;
-                var g = panels[i];
-                if (g.activeSelf != active) g.SetActive(active);
-            }
+            var active = i == index;
+            var g = panels[i];
+            if (g.activeSelf != active) g.SetActive(active);
         }
+    }
 
-        void OnEnable()
-        {
-            SetActivePanel(0);
-        }
+    void OnEnable()
+    {
+        SetActivePanel(0);
     }
 }
--- Assets/Scripts/MetaGameController.cs
+++ Assets/Scripts/MetaGameController.cs
@@ -1,73 +1,69 @@
-//using Platformer.Mechanics;
-//using Platformer.UI;
 using UnityEngine;
 
-namespace Platformer.UI
+
+/// <summary>
+/// The MetaGameController is responsible for switching control between the high level
+/// contexts of the application, eg the Main Menu and Gameplay systems.
+/// </summary>
+public class MetaGameController : MonoBehaviour
 {
     /// <summary>
-    /// The MetaGameController is responsible for switching control between the high level
-    /// contexts of the application, eg the Main Menu and Gameplay systems.
+    /// The main UI object which used for the menu.
     /// </summary>
-    public class MetaGameController : MonoBehaviour
+    public MainUIController mainMenu;
+
+    /// <summary>
+    /// A list of canvas objects which are used during gameplay (when the main ui is turned off)
+    /// </summary>
+    public Canvas[] gamePlayCanvasii;
+
+    /// <summary>
+    /// The game controller.
+    /// </summary>
+    //public GameController gameController;
+
+    bool showMainCanvas = false;
+
+    void OnEnable()
     {
-        /// <summary>
-        /// The main UI object which used for the menu.
-        /// </summary>
-        public MainUIController mainMenu;
+        _ToggleMainMenu(showMainCanvas);
+    }
 
-        /// <summary>
-        /// A list of canvas objects which are used during gameplay (when the main ui is turned off)
-        /// </summary>
-        public Canvas[] gamePlayCanvasii;
-
-        /// <summary>
-        /// The game controller.
-        /// </summary>
-        //public GameController gameController;
-
-        bool showMainCanvas = false;
-
-        void OnEnable()
+    /// <summary>
+    /// Turn the main menu on or off.
+    /// </summary>
+    /// <param name="show"></param>
+    public void ToggleMainMenu(bool show)
+    {
+        if (this.showMainCanvas != show)
         {
-            _ToggleMainMenu(showMainCanvas);
+            _ToggleMainMenu(show);
         }
+    }
 
-        /// <summary>
-        /// Turn the main menu on or off.
-        /// </summary>
-        /// <param name="show"></param>
-        public void ToggleMainMenu(bool show)
+    void _ToggleMainMenu(bool show)
+    {
+        if (show)
         {
-            if (this.showMainCanvas != show)
-            {
-                _ToggleMainMenu(show);
-            }
+            Time.timeScale = 0;
+            mainMenu.gameObject.SetActive(true);
+            foreach (var i in gamePlayCanvasii) i.gameObject.SetActive(false);
         }
-
-        void _ToggleMainMenu(bool show)
+        else
         {
-            if (show)
-            {
-                Time.timeScale = 0;
-                mainMenu.gameObject.SetActive(true);
-                foreach (var i in gamePlayCanvasii) i.gameObject.SetActive(false);
-            }
-            else
-            {
-                Time.timeScale = 1;
-                mainMenu.gameObject.SetActive(false);
-                foreach (var i in gamePlayCanvasii) i.gameObject.SetActive(true);
-            }
-            this.showMainCanvas = show;
+            Time.timeScale = 1;
+            mainMenu.gameObject.SetActive(false);
+            foreach (var i in gamePlayCanvasii) i.gameObject.SetActive(true);
         }
+        this.showMainCanvas = show;
+    }
 
-        void Update()
+    void Update()
+    {
+        if (Input.GetButtonDown("Menu"))
         {
-            if (Input.GetButtonDown("Menu"))
-            {
-                ToggleMainMenu(show: !showMainCanvas);
-            }
+            ToggleMainMenu(show: !showMainCanvas);
         }
-
     }
 }
+
